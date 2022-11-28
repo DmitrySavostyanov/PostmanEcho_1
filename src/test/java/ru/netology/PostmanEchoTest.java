092b585f0c1b946a9ca37c17341dc54bcd8c44e9
Dmitry Savostyanov <dokken671@gmail.com>
2022-11-29 00:02:35 +0700

    init commit

---
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -0,0 +1,23 @@
+package ru.netology;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+    @Test
+    public void shouldPostRawTextTest() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("Это мой первый API автотест?")
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Это мой первый API автотест?"));
+    }
+
+
+}
